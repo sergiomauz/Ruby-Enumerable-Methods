a45b4a154ea19432dd0ac8aea4429535dcaa7312
Sergio <sergio.mauz88@gmail.com>
2020-03-11 02:53:32 -0500

    Added my_select function

--- enumerable.rb
+++ enumerable.rb
@@ -1,22 +1,28 @@
 module Enumerable
     def my_each
-            i = 0
-            while i < self.length
-                yield(self[i])
-                i+=1
-            end
+        i = 0
+        while i < self.length
+            yield(self[i])
+            i+=1
+        end
     end
     
     def my_each_with_index
-            i = 0
-            while i < self.length
-                yield(i, self[i])
-                i += 1
-            end
+        i = 0
+        while i < self.length
+            yield(i, self[i])
+            i += 1
+        end
     end
 
     def my_select       
-       
+        narray = []
+        self.my_each do |v| 
+            if yield(v)
+                narray << v
+            end
+        end       
+        narray
     end
 
     def my_all?()
@@ -48,3 +54,8 @@ module Enumerable
     end
 end
 
+a = [-2, 2, 4, 0, -1]
+b = a.my_select do |v|
+    v > 0
+end
+p b
