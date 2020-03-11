124764a72cb69a09e96bcf907e5caba45fd62fbd
Sergio <sergio.mauz88@gmail.com>
2020-03-10 22:14:10 -0500

    Added my_each function

---
+++ enumerable.rb
@@ -0,0 +1,47 @@
+module Enumerable
+    def my_each(arr)  
+        if !arr.nil?
+            i = 0
+            while i < arr.length
+                yield(arr[i])
+                i += 1
+            end    
+        end
+    end
+
+    def my_each_with_index(arr)
+
+    end
+
+    def my_select()
+
+    end
+
+    def my_all?()
+
+    end
+
+    def my_any?()
+
+    end
+
+    def my_none?()
+
+    end
+
+    def my_count()
+
+    end
+
+    def my_map()
+
+    end
+
+    def my_inject()
+
+    end
+
+    def multiply_els()
+
+    end
+end
