0fb7535553558f0ead3aa4c97a0238c53f7d5999
Sergio <sergio.mauz88@gmail.com>
2020-03-15 15:52:46 -0500

    Added rubocop

--- enumerable.rb
+++ enumerable.rb
@@ -1,62 +1,52 @@
+# Enumerable methods
 module Enumerable
-    def my_each
-        i = 0
-        while i < self.length
-            yield(self[i])
-            i+=1
-        end
+  def my_each(&codeb)
+    return to_enum(:my_each) unless block_given?
+
+    i = 0
+    while i < length
+      if is_a?(Array)
+        yield(self[i])
+      elsif codeb.arity == 1
+        yield(assoc keys[i])
+      else
+        yield(keys[i], self[keys[i]])
+      end
+      i += 1
     end
-    
-    def my_each_with_index
-        i = 0
-        while i < self.length
-            yield(i, self[i])
-            i += 1
-        end
+    self
+  end
+
+  def my_each_with_index(&codeb)
+    return to_enum(:my_each_with_index) unless block_given?
+
+    i = 0
+    while i < length
+      if is_a?(Array)
+        yield(self[i], i)
+      elsif codeb.arity == 1
+        yield(assoc keys[i])
+      else
+        yield([keys[i], self[keys[i]]], i)
+      end
+      i += 1
     end
+    self
+  end
 
-    def my_select       
-        narray = []
-        self.my_each do |v| 
-            if yield(v)
-                narray << v
-            end
-        end       
-        narray
+  def my_select
+    if is_a?(Array)
+      new_array = []
+      my_each do |v|
+        new_array << v if yield(v)
+      end
+      new_array
+    else
+      new_hash = Hash[]
+      my_each do |k, v|
+        new_hash[k] = v if yield(k, v)
+      end
+      new_hash
     end
-
-    def my_all?
-        r = true
-        self.my_each do |v| 
-            if !yield(v)
-                r = false
-                break
-            end
-        end
-        r
-    end
-
-    def my_any?()
-
-    end
-
-    def my_none?()
-
-    end
-
-    def my_count()
-
-    end
-
-    def my_map()
-
-    end
-
-    def my_inject()
-
-    end
-
-    def multiply_els()
-
-    end
+  end
 end
