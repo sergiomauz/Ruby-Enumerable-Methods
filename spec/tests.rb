1c46f89b876e4624907eab3c994d43a4f5e1714d
peterrobert <pwambui93@gmail.com>
2020-03-31 19:24:00 +0300

    testing the emurables methods

---
+++ spec/tests.rb
@@ -0,0 +1,12 @@
+# rubocop: disable Layout/LineLength
+
+require_relative '../enumerable.rb'
+
+RSpec.describe Enumerable do
+  describe '#my_each' do
+    
+   
+  end
+end
+
+# rubocop: enable Layout/LineLength
