d0d9f279da3d667a2ed6bb6dcac3c89e9036b14f
Sergio <sergio.mauz88@gmail.com>
2020-04-03 11:23:55 -0500

    Fixed my_each testing

--- spec/tests.rb
+++ spec/tests.rb
@@ -1,12 +1,25 @@
-# rubocop: disable Layout/LineLength
-
+# Tests.rb
 require_relative '../enumerable.rb'
 
 RSpec.describe Enumerable do
+  array_numbers = [1, 5, 9, 8, 7, 3, 4]
+  hash_numbers = { 'a' => 3, 'b' => 7, 'c' => 9, 'd' => 8, 'e' => 6, 'f' => 2 }
+
   describe '#my_each' do
-    
-   
+    it 'If we pass an array with a block, it will returns the same array' do
+      expect(array_numbers.my_each {}).to eql(array_numbers)
+    end
+
+    it 'If we pass a hash with a block, it will returns the same hash' do
+      expect(hash_numbers.my_each {}).to eql(hash_numbers)
+    end
+
+    it 'If we pass an array without a block, it will returns a kind of Enumerator' do
+      expect(array_numbers.my_each).to be_kind_of(Enumerator)
+    end
+
+    it 'If we pass hash without a block, it will returns a kind of Enumerator' do
+      expect(hash_numbers.my_each).to be_kind_of(Enumerator)
+    end
   end
 end
-
-# rubocop: enable Layout/LineLength
